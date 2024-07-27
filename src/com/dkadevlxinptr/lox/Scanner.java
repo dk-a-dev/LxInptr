ecedd9f56a8ae6a9ec41566ec60e5f47eef1e690
Dev Keshwani <dev.keshwani345@gmail.com>
2024-07-27 16:09:53 +0530

    feat: keyword and variable idetification logic added

--- src/com/dkadevlxinptr/lox/Scanner.java
+++ src/com/dkadevlxinptr/lox/Scanner.java
@@ -14,6 +14,27 @@ class Scanner {
     private int current = 0;
     private int line = 1;
 
+    private static final Map<String, TokenType> keywords;
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
+    }
+
     Scanner(String source) {
         this.source = source;
     }
@@ -102,12 +123,26 @@ class Scanner {
             default:
                 if (isDigit(c)) {
                     number();
+                } else if (isAlpha(c)) {
+                    identifier();
                 } else {
                     Lox.error(line, "Unexpected character.");
                 }
                 break;
         }
     }
+
+    // Identifier and Keyword Helper Function
+    private void identifier() {
+        while (isAlphaNumeric(peek()))
+            advance();
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null)
+            type = IDENTIFIER;
+        addToken(type);
+    }
+
     // Numeric Literal Helper Functions
     private void number() {
         while (isDigit(peek()))
@@ -126,6 +161,16 @@ class Scanner {
         if (current + 1 >= source.length())
             return '\0';
         return source.charAt(current + 1);
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                c == '_';
     }
 
     private boolean isDigit(char c) {
@@ -164,7 +209,7 @@ class Scanner {
         return true;
     }
 
-    // advance to next char 
+    // advance to next char
     private char advance() {
         return source.charAt(current++);
     }
@@ -173,6 +218,7 @@ class Scanner {
     private void addToken(TokenType type) {
         addToken(type, null);
     }
+
     private void addToken(TokenType type, Object literal) {
         String text = source.substring(start, current);
         tokens.add(new Token(type, text, literal, line));
